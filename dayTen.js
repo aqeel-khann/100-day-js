f7d1670a35bbe6fe2f323380d2790240af2f0ef3
Muhammad Aqeel <khanaqeel6611@gmail.com>
2024-05-30 23:35:31 +0500

    day ten

--- dayTen.js
+++ dayTen.js
@@ -1,5 +1,11 @@
 function sumOfDigit(str) {
-    strArr = str.split(" ")
-    console.log(strArr);
+//     let myStr = str.toString().split("").map(Number).reduce((acc, curval) => acc + curval, 0)
+    //    return myStr
+    let sum = 0
+    while (str) {
+        sum += str % 10
+        str=Math.floor(str/10)
+    }
+    return sum
 }
-console.log(sumOfDigit("123"));
+console.log(sumOfDigit(123));
